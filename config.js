417df784f30c0d49c326481f7b396d2e66186bde
Duy Nguyen Quang <quangduya4@gmail.com>
2020-04-19 15:40:09 +0700

    Change to github page

--- config.js
+++ config.js
@@ -6,6 +6,6 @@
 
 var vars = {
     file : 'index.html',
-    path : 'http://localhost',//path to the index.html
+    path : 'https://duynqvn.github.io/rcong/',//path to the index.html
     theme : 'theasys',
 };
